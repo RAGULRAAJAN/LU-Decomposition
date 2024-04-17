d512a0f825162d57f473cd73dcb057ce52164159
RAGULRAAJAN <ragul18012006@gmail.com>
2024-04-17 16:48:21 +0530

    212223100043

--- LU.py
+++ LU.py
@@ -1,14 +1,23 @@
 # To print L and U matrix
+'''Program to find L and U matrix using LU decomposition.
+Developed by: RAGUL RAAJAN T
+RegisterNumber: 212223100043
+'''
 import numpy as np
-#add the library to import lu
-A = np.array(eval(input()))
-#add the code to get the L and U matrix
+from scipy.linalg import lu
+A=np.array(eval(input()))
+P,L,U=lu(A)
+print(L)
+print(U)
+
 
 
 # To print X matrix (solution to the equations)
 import numpy as np
-#add the library to import lu_factor, lu_solve
-A = np.array(eval(input()))
-b = np.array(eval(input()))
-#add the code to get the solution of the matrix
+from scipy.linalg import lu_factor,lu_solve
+A=np.array(eval(input()))
+B=np.array(eval(input()))
+lu,piv=lu_factor(A)
+x=lu_solve((lu,piv),B)
+print(x)
 
